bc233fd69d2f7e81c39ef0e1308bcd9e4720282b
Dan O'Huiginn <daniel@ohuiginn.net>
2023-11-11 12:41:44 +0100

    first working version

--- factcheck_helper.py
+++ factcheck_helper.py
@@ -3,11 +3,36 @@ import llm
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+# replace with your own key
+from secrets import openai_api_key
 
 app = FastAPI()
 
-model = llm.get_model("gpt-3.5-turbo")
-model.key = 
+model = llm.get_model("gpt-4")
+model.key = openai_api_key
+
+base_prompt = '''
+Your job is to assist a newspaper fact-checker. Read the following draft article, and compile a list of assertions which need to be proved.
+List the assertions one per line. Write the assertion in a terse style, omitting stylistic details.
+Each assertion should contain a single claim that can be checked independently.
+Rewrite each assertion so that it can be understood independently -- for example, replace pronouns by the name of the person they refer to.
+Write at least 20 assertions.
+Do not compile multiple claims in a single line.
+Do not include anything else in your response.
+The article follows:
+
+'''
+
+def get_openai_response(user_prompt):
+    full_prompt = base_prompt + user_prompt
+    response = model.prompt(full_prompt, max_tokens=1000).text()
+    print(response)
+    return response.split('\n')
+
+class FactCheckRequest(BaseModel):
+    article: str
 
 @app.get("/")
 async def root():
@@ -16,12 +41,11 @@ async def root():
     # return HTMLResponse(content=content)
 
 @app.post('/factcheck')
-async def factcheck():
+async def factcheck(request_data: FactCheckRequest):
+    article_text = request_data.article
+    response = get_openai_response(article_text)
     sample =   {
-        'claims': [
-            'what is your name?',
-            'what is your quest?',
-        ]
+        'claims': response
     }
     return JSONResponse(content=sample)
 
